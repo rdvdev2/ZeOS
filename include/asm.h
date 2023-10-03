a57109fd0f46971dc83a46caf88d8077c252d4ba
Nara Díaz Viñolas <rdvdev2@gmail.com>
2023-10-03 07:26:27 +0000

    Move system_call_handler to entry.S

--- include/asm.h
+++ include/asm.h
@@ -11,37 +11,4 @@
     .align 0; \
   name:
 
-#define SAVE_ALL \
-      pushl %gs; \
-      pushl %fs; \
-      pushl %es; \
-      pushl %ds; \
-      pushl %eax; \
-      pushl %ebp; \
-      pushl %edi; \
-      pushl %esi; \
-      pushl %ebx; \
-      pushl %ecx; \
-      pushl %edx; \
-      movl $__KERNEL_DS, %edx;    \
-      movl %edx, %ds;           \
-      movl %edx, %es
-
-#define RESTORE_ALL \
-      popl %edx; \
-      popl %ecx; \
-      popl %ebx; \
-      popl %esi; \
-      popl %edi; \
-      popl %ebp; \
-      popl %eax; \
-      popl %ds; \
-      popl %es; \
-      popl %fs; \
-      popl %gs;
-
-#define EOI \
-      movb $0x20, %al; \
-      outb %al, $0x20;
-
 #endif  /* __ASM_H__ */
